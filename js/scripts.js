d7d8cec62819a01565e9ed07c80f5907ec0a8e16
oliver fu <ofu997@gmail.com>
2017-03-03 10:52:09 -0800

    Add ability to pass toppings to constructor and console log them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
-function Pizza(inputSize, inputToppings){
-  this.Size = inputSize;
+function Pizza(inputToppings){
+  //this.Size = inputSize;
   this.Toppings = inputToppings;
-}
 
-Pizza.prototype.fullDetails = function(){
-  return "You ordered a " + this.Size + "pizza with the following toppings" + this.Toppings;
 }
+//
+// Pizza.prototype.fullDetails = function(){
+//   return "You ordered a " + + "pizza with the following toppings" + this.Toppings;
+// }
 
 
 $(function(){
   $("form#form").submit(function(event) {
   event.preventDefault();
-  var inputSize = $("input#size").val();
-  var inputToppings = $("input#toppings").val();
 
-  var newPizza = new Pizza(inputSize, inputToppings);
-  $("#output").append(newPizza.fullDetails);
+  //var inputSize = $("input#size").val();
+  //var inputToppings = $("input#toppings").val();
+
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var inputToppings = $(this).val();
+      //$("#answers").append(inputToppings);
+      // var inputToppings;
+      var newPizza = new Pizza(inputToppings);
+      //console.log(newPizza);
+      console.log(newPizza.Toppings);
+    });
+  // var inputToppings;
+  // var newPizza = new Pizza(inputToppings);
+
+  //$("#output").append(newPizza.fullDetails);
   });
 })
